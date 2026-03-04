aafe8511ec53964bc295423deb9a3a608d33c77c
Morishey <morishey@onmail.com>
2026-03-04 17:29:18 +0700

    4000

--- src/components/SendMoney.tsx
+++ src/components/SendMoney.tsx
@@ -408,7 +408,7 @@ const SendMoney: React.FC = () => {
         setIsLoading(true);
         setLoaderMessage('Processing please wait...');
 
-        await new Promise(resolve => setTimeout(resolve, 3000));
+        await new Promise(resolve => setTimeout(resolve, 4000));
         setLoaderMessage('Fetching data...');
 
         await new Promise(resolve => setTimeout(resolve, 1000));
